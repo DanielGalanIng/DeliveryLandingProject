ed3636970cf6b68660393430cebbeefca3831d0c
Daniel Galán <danielgalan0410@icloud.com>
2021-11-23 19:18:08 -0500

    scroll button updated

--- js/main.js
+++ js/main.js
@@ -22,3 +22,43 @@ function linkAction() {
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+//Scroll sections active link
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+  const scrollY = window.scrollY
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+          sectionTop = current.offsetTop - 50,
+          sectionId = current.getAttribute('id')
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+    }else{
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
+
+//Change background header 
+
+function scrollHeader() {
+  const header = document.getElementById('header')
+  if (window.scrollY > 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
+
+//Show scroll up button
+
+function showScroll() {
+  const scrollUp = document.getElementById('scroll-up')
+  if (window.scrollY > window.innerHeight) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', showScroll)
